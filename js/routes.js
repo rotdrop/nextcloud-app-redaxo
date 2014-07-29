451cf9d27a21c5801169d131378002d8c0347b4a
Claus-Justus Heine <himself@claus-justus-heine.de>
2014-07-29 10:08:18 +0200

    Reenable refreshing.

--- js/routes.js
+++ js/routes.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
-//    OC.Router.registerLoadedCallback(function(){
-//        var url = OC.Router.generate('dokuwikirefresh');
-//        setInterval(function(){
-//            $.post(url);
-//        }, 300000);
-//    });
+    var url = OC.generateUrl('apps/dokuwikiembed/refresh');
+    setInterval(function(){
+        $.post(url);
+    }, 300000);
 });
