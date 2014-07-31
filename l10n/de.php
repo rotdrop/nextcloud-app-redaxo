8941b91577101a6a043b7a118d8cb64c16b306d2
Claus-Justus Heine <himself@claus-justus-heine.de>
2014-07-31 21:14:43 +0200

    Update translations.

--- l10n/de.php
+++ l10n/de.php
@@ -6,6 +6,6 @@
 "DokuWiki" => "DokuWiki",
 "Embedded DokuWiki" => "Eingebettetes DokuWiki",
 "Location" => "Location",
-"Please enter the location of the already installed DokuWiki\ninstance. This should either be an abolute path relative to the\nroot of the web-browser, or a complete URL which points to the\nweb-location of the DokuWiki. In order to make things work your\nhave to enable the XMLRPC protocol in your DokuWiki." => "Bitte geben Sie den Ort der DokuWiki-Installation an.\nEntweder als \"absoluten\" Pfad, aber relativ zum Basis-Verzeichnis\nIhres Web-Servers, oder als vollständigen URL mit der Web-Adresse\nder DokuWiki-Installation. Sie müssen im DokuWiki das\nXMLRPC-Protokoll aktivieren",
+"Please enter the location of the already installed DokuWiki\ninstance. This should either be an abolute path relative to the\nroot of the web-browser, or a complete URL which points to the\nweb-location of the DokuWiki. In order to make things work your\nhave to enable the XMLRPC protocol in your DokuWiki." => "Bitte geben Sie den Ort der DokuWiki-Installation an.\nEntweder als ``absoluten'' Pfad, aber relativ zum Basis-Verzeichnis\nIhres Web-Servers, oder als vollständigen URL mit der Web-Adresse\nder DokuWiki-Installation. Sie müssen im DokuWiki das\nXMLRPC-Protokoll aktivieren",
 "DokuWiki Location" => "Dokuwiki Location"
 );
