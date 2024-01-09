b5b587bca34a747e90ca274831ecf9bba5313063
Claus-Justus Heine <himself@claus-justus-heine.de>
2024-01-09 14:53:37 +0100

    Update assets.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'rotdrop/redaxo',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '99b2a8fb63d61b066e0768a254554b9a3571d846',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'myclabs/php-enum' => array(
+            'pretty_version' => '1.8.4',
+            'version' => '1.8.4.0',
+            'reference' => 'a867478eae49c9f59ece437ae7f9506bfaa27483',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../myclabs/php-enum',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'rotdrop/redaxo' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '99b2a8fb63d61b066e0768a254554b9a3571d846',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
