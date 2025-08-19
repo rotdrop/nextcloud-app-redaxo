210d4150da211c11c876560dbf99c006280d6c0f
Claus-Justus Heine <himself@claus-justus-heine.de>
2025-08-19 16:49:39 +0200

    cloud-version-classes.js -> .tx

--- src/AdminSettings.vue
+++ src/AdminSettings.vue
@@ -66,7 +66,7 @@ import { appName } from './config.ts'
 import { translate as t } from '@nextcloud/l10n'
 import { NcSettingsSection } from '@nextcloud/vue'
 import TextField from '@rotdrop/nextcloud-vue-components/lib/components/TextFieldWithSubmitButton.vue'
-import cloudVersionClassesImport from './toolkit/util/cloud-version-classes.js'
+import cloudVersionClassesImport from './toolkit/util/cloud-version-classes.ts'
 import {
   fetchSettings,
   saveConfirmedSetting,
